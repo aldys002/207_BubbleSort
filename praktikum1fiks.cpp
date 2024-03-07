499f8e106591ba8d1cb884f11a580c3b9588f8dc
aldys002 <igidiaaldys@gmail.com>
2024-03-07 17:28:36 +0700

    menambah prosedur bubblesortArray

--- praktikum1fiks.cpp
+++ praktikum1fiks.cpp
@@ -21,4 +21,25 @@ void input() {
 	cout << "===============" << endl;
 	cout << "masukan Elemen Array" << endl;
 	cout << "===============" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "data ke - " << (i + 1) << ":";
+		cin >> arr[i];
+	}
+}
+
+void bubbleSortArray() { //prosedur untuk mengurutkan array dengan metode bubble sort
+	int pass = 1; //step 1
+	do {
+		for (int j = 0; j <= n - 1 - pass; j++) { //step 2
+			if (arr[j] > arr[j + 1]) {//step 3
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+
+			}
+		}
+		pass = pass + 1; //step 4
+	} while (pass <= n); //step 5
+
 }
